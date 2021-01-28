7dcf24658f8e7c82a58d9c7b3d1c5a53b8892c53
marce941 <61011864+marce941@users.noreply.github.com>
2021-01-28 14:35:02 -0300

    Add files via upload

---
+++ Inicio.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Inicio
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.btnEditar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtNumero = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtDireccion = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(6, 26)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(282, 304)
+        Me.ListBox1.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(294, 349)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Listado"
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.Location = New System.Drawing.Point(13, 367)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(94, 29)
+        Me.btnNuevo.TabIndex = 2
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'btnEditar
+        '
+        Me.btnEditar.Location = New System.Drawing.Point(113, 367)
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(94, 29)
+        Me.btnEditar.TabIndex = 3
+        Me.btnEditar.Text = "Editar"
+        Me.btnEditar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(213, 367)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(94, 29)
+        Me.btnEliminar.TabIndex = 4
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(10, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 20)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Nombre"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(169, 32)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(263, 27)
+        Me.txtNombre.TabIndex = 6
+        '
+        'txtNumero
+        '
+        Me.txtNumero.Location = New System.Drawing.Point(169, 87)
+        Me.txtNumero.Name = "txtNumero"
+        Me.txtNumero.Size = New System.Drawing.Size(125, 27)
+        Me.txtNumero.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(10, 90)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(134, 20)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Número teléfonico"
+        '
+        'txtDireccion
+        '
+        Me.txtDireccion.Location = New System.Drawing.Point(169, 149)
+        Me.txtDireccion.Name = "txtDireccion"
+        Me.txtDireccion.Size = New System.Drawing.Size(263, 27)
+        Me.txtDireccion.TabIndex = 10
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 152)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 20)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Dirección"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.txtNombre)
+        Me.GroupBox2.Controls.Add(Me.txtDireccion)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.txtNumero)
+        Me.GroupBox2.Location = New System.Drawing.Point(312, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(452, 211)
+        Me.GroupBox2.TabIndex = 13
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Datos"
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Location = New System.Drawing.Point(670, 229)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(94, 29)
+        Me.btnCancelar.TabIndex = 14
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(494, 301)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(94, 29)
+        Me.btnGuardar.TabIndex = 15
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'Inicio
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(778, 409)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnEditar)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Inicio"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Inicio"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents btnEditar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtNumero As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtDireccion As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnCancelar As Button
+    Friend WithEvents btnGuardar As Button
+End Class
---
+++ Inicio.vb
@@ -0,0 +1,178 @@
+﻿Imports System.Data.SqlClient
+Public Class Inicio
+    Public id As Integer
+    Public ultimo As Integer
+    Public bandera As Integer
+    Public cod As Integer
+
+    Private Sub Inicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnEditar.Enabled = False
+        txtNombre.Enabled = False
+        txtNumero.Enabled = False
+        txtDireccion.Enabled = False
+        btnEliminar.Enabled = False
+        ListBox1.Items.Clear()
+        llenarLista()
+    End Sub
+
+    Private Sub llenarLista()
+        ListBox1.Items.Clear()
+        Dim conexion As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & "D:\Visual Studio 2019\prueba\Database.mdf" & ";Integrated Security=True")
+        Dim cmd As New SqlCommand
+        Dim reader As SqlDataReader
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "Select nombre from agenda"
+        cmd.Connection = conexion
+        Try
+            conexion.Open()
+            reader = cmd.ExecuteReader
+            If reader.HasRows Then
+                While reader.Read
+                    ListBox1.Items.Add(reader(0))
+                End While
+            Else
+                ListBox1.Items.Add("No hay registros para mostrar")
+            End If
+            conexion.Close()
+        Catch ex As Exception
+            MsgBox(ex.ToString, MsgBoxStyle.Critical, "Atención")
+        End Try
+    End Sub
+
+    Private Sub buscarultireg()
+        Dim sql As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & "D:\Visual Studio 2019\prueba\Database.mdf" & ";Integrated Security=True")
+        Dim cmd As New SqlCommand
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "Select top 1 * from agenda order by [id] desc"
+        cmd.Connection = sql
+        Try
+            sql.Open()
+            id = Convert.ToInt32(cmd.ExecuteScalar)
+            sql.Close()
+            ultimo = id + 1
+        Catch ex As Exception
+            MsgBox(ex.ToString, MsgBoxStyle.Critical, "Atención")
+        End Try
+    End Sub
+
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+        txtNombre.Text = ""
+        txtNumero.Text = ""
+        txtDireccion.Text = ""
+        txtNombre.Enabled = False
+        txtNumero.Enabled = False
+        txtDireccion.Enabled = False
+        btnEditar.Enabled = False
+        btnNuevo.Enabled = True
+        btnEliminar.Enabled = False
+
+    End Sub
+
+    Private Sub btnEditar_Click(sender As Object, e As EventArgs) Handles btnEditar.Click
+        txtNombre.Enabled = True
+        txtNumero.Enabled = True
+        txtDireccion.Enabled = True
+        txtNombre.Focus()
+        btnEditar.Enabled = False
+        btnNuevo.Enabled = False
+        btnEliminar.Enabled = False
+        bandera = 2
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        btnEditar.Enabled = True
+        btnEliminar.Enabled = True
+        Dim a As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & "D:\Visual Studio 2019\prueba\Database.mdf" & ";Integrated Security=True")
+        Dim b As New SqlCommand
+        Dim lector As SqlDataReader
+        b.CommandType = CommandType.Text
+        b.CommandText = "select  id, nombre, numero, direccion from agenda where nombre ='" & ListBox1.SelectedItem & "'"
+        b.Connection = a
+        Try
+            a.Open()
+            lector = b.ExecuteReader
+            If lector.HasRows Then
+                While lector.Read
+                    cod = lector(0)
+                    txtNombre.Text = lector(1)
+                    txtNumero.Text = lector(2)
+                    txtDireccion.Text = lector(3)
+                End While
+            End If
+            buscarultireg()
+        Catch ex As Exception
+            MsgBox(ex.ToString, MsgBoxStyle.Critical, "Atención")
+        End Try
+    End Sub
+
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        txtNombre.Enabled = True
+        txtNumero.Enabled = True
+        txtDireccion.Enabled = True
+        txtNombre.Focus()
+        bandera = 1
+        btnNuevo.Enabled = False
+        btnEditar.Enabled = False
+        btnEliminar.Enabled = False
+        buscarultireg()
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        If MsgBox("¿Desea eliminar el registro " & ListBox1.SelectedItem.ToString & "?", MsgBoxStyle.YesNo, "Pregunta") Then
+            Try
+                Dim s As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & "D:\Visual Studio 2019\prueba\Database.mdf" & ";Integrated Security=True")
+                Dim c As New SqlCommand
+                c.CommandType = CommandType.Text
+                c.CommandText = "delete from agenda where nombre ='" & ListBox1.SelectedItem & "'"
+                c.Connection = s
+                s.Open()
+                c.ExecuteNonQuery()
+                s.Close()
+                llenarLista()
+                btnCancelar.PerformClick()
+            Catch ex As Exception
+                MsgBox(ex.ToString, MsgBoxStyle.Critical, "Atención")
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        If bandera = 1 Then
+            Dim sql As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & "D:\Visual Studio 2019\prueba\Database.mdf" & ";Integrated Security=True")
+            Dim cmd As New SqlCommand
+            Try
+                cmd.CommandType = CommandType.Text
+                cmd.CommandText = "insert into agenda values (@id,@nombre,@numero,@direccion)"
+                cmd.Parameters.AddWithValue("@id", ultimo)
+                cmd.Parameters.AddWithValue("@nombre", txtNombre.Text)
+                cmd.Parameters.AddWithValue("@numero", txtNumero.Text)
+                cmd.Parameters.AddWithValue("@direccion", txtDireccion.Text)
+                cmd.Connection = sql
+                sql.Open()
+                cmd.ExecuteNonQuery()
+                sql.Close()
+                llenarLista()
+            Catch ex As Exception
+                MsgBox(ex.ToString, MsgBoxStyle.Critical, "Atención")
+            End Try
+        ElseIf bandera = 2 Then
+            Dim conec As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" & "D:\Visual Studio 2019\prueba\Database.mdf" & ";Integrated Security=True")
+            Dim comand As New SqlCommand
+            Try
+                comand.CommandType = CommandType.Text
+                comand.CommandText = "update agenda set [nombre]=@nombre, [numero]=@numero, [direccion]=@direccion where id = " & cod
+                comand.Parameters.AddWithValue("@nombre", txtNombre.Text)
+                comand.Parameters.AddWithValue("@numero", txtNumero.Text)
+                comand.Parameters.AddWithValue("@direccion", txtDireccion.Text)
+                comand.Connection = conec
+                conec.Open()
+                comand.ExecuteNonQuery()
+                conec.Close()
+                llenarLista()
+            Catch ex As Exception
+                MsgBox(ex.ToString, MsgBoxStyle.Critical, "Atención")
+            End Try
+        End If
+        btnCancelar.PerformClick()
+    End Sub
+End Class
